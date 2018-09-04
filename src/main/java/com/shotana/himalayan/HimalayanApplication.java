5fa5f4b04043d4035e4da14e6a09eb271495923f
田中将平 <syouhei@jobby-MBP.local>
2018-09-04 20:00:50 +0900

    Initial

---
+++ src/main/java/com/shotana/himalayan/HimalayanApplication.java
@@ -0,0 +1,12 @@
+package com.shotana.himalayan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimalayanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HimalayanApplication.class, args);
+    }
+}
